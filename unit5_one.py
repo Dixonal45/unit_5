189f38beda0fb5e8f8feb39cc618269bd7228f31
allison <allison.dixon@ssfs.org>
2019-10-22 15:05:09 -0400

    finished unit 5 project option 1

--- unit5_one.py
+++ unit5_one.py
@@ -1,24 +1,39 @@
 # by Allison Dixon
-# last updated October 21, 2019
+# last updated October 22, 2019
 # this program plays a number guessing game with the user
 
 import random
 
 
 def main():
+    # this while True statement makes it possible for the user to play multiple times
     while True:
-        answer = input("Would you like to play a game? Answer 'y' for yes or 'n' for no.")
-        if answer == "y" or answer == "n":
+        # this while True statement asks and responds to if the user wants to play the game
+        while True:
+            answer = input("Would you like to play a game? Answer 'y' for yes or 'n' for no.")
+            if answer == "y" or answer == "n":
+                break
+        if answer == "y":
+            print("Let's play.")
+        else:
+            print("bye bye")
             break
-    if answer == "y":
-        print("Let's play.")
-    else:
-        for x in range(100):
-            print("TERMINATE")
+        computer_num = random.randint(1, 100)
+        tries = 0
+        # this while True statement allows the user to guess the number and responds; it also counts the tries
+        while True:
+            guess = int(input("Guess the computer's number between one and a hundred."))
+            tries += 1
+            if computer_num == guess:
+                print("Way to go, you got it right.")
+                break
+            elif guess >= 100 or guess <= 0:
+                print("Make sure your guess is in the right range.")
+            elif guess > computer_num:
+                print("Too high.")
+            else:
+                print("Too low.")
+        print("It took you", tries, "to guess the number.")
 
-
-computer_num = random.randint(1, 100)
-
-int(input("Guess the computer's number"))
 
 main()
